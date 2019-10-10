d8e5cec2091f59c3d8ad93a1fb908b3ffcad8732
N/A <ruben.wood1@gmail.com>
2019-10-10 16:22:49 +0100

    Initial Commit (10/10/2019)
    Initial Commit for repo

---
+++ Assets/Scripts/VBPlayAnim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VBPlayAnim : MonoBehaviour, IVirtualButtonEventHandler 
+{
+    public GameObject model;
+    private Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = model.GetComponent<Animator>();
+
+        /// Register the event handler
+        GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button Pressed!");       
+
+        anim.Play("Expand");
+
+        ///throw new System.NotImplementedException();
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button Released!");
+
+        anim.Play("None");
+
+        ///throw new System.NotImplementedException();
+    }
+}
